fb90bdc90b07d56ed2481f8a69d59d856304e609
komalkhatria <komalkhatria29@gmail.com>
2022-12-27 19:41:45 +0530

    Description added

--- about.php
+++ about.php
@@ -1,1 +1,12 @@
 <!-- This website "Khaana" will offer Indian food which gives the customers 'Ghar ka khaana wali feeling' -->
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>
+            About Khaana...
+        </h1>
+    </body>
+</html>
