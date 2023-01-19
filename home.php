1ab49080879ad0db0f844b790bbef712943d6906
komalkhatria <komalkhatria29@gmail.com>
2023-01-19 16:23:39 +0530

    home slider added

--- home.php
+++ home.php
@@ -22,6 +22,8 @@
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
     <!-- custom css file link -->
     <link rel="stylesheet" type="text/css" href="style.css">
 
@@ -33,7 +35,52 @@
     <!-- header section ends -->
 
     <!-- home section starts -->
+    <section class="home">
 
+        <div class="swiper home-slider">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide slide">
+                    <div class="content">
+                        <span>order online</span>
+                        <h3>delicious khaman</h3>
+                        <a href="menu.php" class="btn">see menus</a>
+                    </div>
+                    <div class="image">
+                        <img src="images/khaman_dhokla.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="content">
+                        <span>order online</span>
+                        <h3>healthy khichdi</h3>
+                        <a href="menu.php" class="btn">see menus</a>
+                    </div>
+                    <div class="image">
+                        <img src="images/dal-khichadi.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="content">
+                        <span>order online</span>
+                        <h3>tasty halwa</h3>
+                        <a href="menu.php" class="btn">see menus</a>
+                    </div>
+                    <div class="image">
+                        <img src="images/suji-halwa.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="swiper-pagination"></div>
+
+            </div>
+
+        </div>
+
+    </section>
     <!-- home section ends -->
 
 
@@ -58,9 +105,23 @@
     <?php include ('footer.php') ?>
     <!-- footer section ends -->
 
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     
     <!-- custom js file link -->
     <script src="script.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper(".home-slider", {
+        effect: "flip",
+        grabCursor: true,
+        loop: true,
+        pagination: {
+            clickable: true,
+            el: ".swiper-pagination",
+        },
+      });
+    </script>
 
 
 
