318446f68a7a3c7aa918b5533aec78e3e8c6db27
komalkhatria <komalkhatria29@gmail.com>
2023-01-19 16:28:37 +0530

    files commited

---
+++ update_address.php
@@ -0,0 +1,65 @@
+<?php
+    include('connect.php');
+
+    session_start();
+    if (isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user_id'];
+    }
+    else{
+        $user_id = '';
+        header('location: home.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update address</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    
+</head>
+<body>
+    <!-- header section starts -->
+    <?php include ('user_header.php') ?>
+    <!-- header section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer section starts -->
+    <?php include ('footer.php') ?>
+    <!-- footer section ends -->
+
+    
+    <!-- custom js file link -->
+    <script src="script.js"></script>
+
+
+
+</body>
+</html>
